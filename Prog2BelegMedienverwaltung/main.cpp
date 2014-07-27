c5c813580ef2f826341ba67a6cacae97e2e52840
Markus Klemm <markus@markusklemm.net>
2014-07-27 19:44:49 +0200

    Initial Commit

---
+++ Prog2BelegMedienverwaltung/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Prog2BelegMedienverwaltung
+//
+//  Created by Markus Klemm on 27.07.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
