9bd3cf48ea58a4354c597565d8f69916271c61ed
Markus Klemm <markus@markusklemm.net>
2016-06-26 20:42:40 +0200

    Forgot to add the moved files to git

---
+++ src/Medium.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Markus Klemm on 22.06.16.
+//
+
+#include "Medium.h"
+
+Medium::Medium(std::string uid)
+: uid(uid)
+{ }
+
+std::string Medium::get_Description() const {
+
+    return {};
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QtGui/QApplication>
+#include <QDeclarativeView>
+
+
+int main(int argc, char* argv[]) {
+    QApplication this_application(argc,argv);
+
+    QDeclarativeView view ;
+    view.setSource(QUrl::fromLocalFile("main_window.qml"));
+    view.show();
+
+    return this_application.exec();
+}
