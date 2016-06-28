ecedaeceed2b9280be466a44ffd1eb04d8f65309
Markus Klemm <markus@markusklemm.net>
2016-06-28 15:14:25 +0200

    Added some person tests

--- test/core_logic_tests.cpp
+++ test/core_logic_tests.cpp
@@ -1,16 +1,23 @@
 #include <QtTest/QtTest>
 
-class TestQString : public QObject {
+#include "../src/person.h"
+
+class core_logic_tests : public QObject {
 Q_OBJECT
 
 private slots:
 
-    void toUpper() {
-        QString str = "Hello";
-        QCOMPARE(str.toUpper(), QString("HELLO"));
+    void person_operators() {
+        person p42(42, L"Mark", L"surter");
+        person p0(0, L"Cathleen", L"Whatever");
+        person p0_almost(0, L"NotCathleen", L"Either");
+        QVERIFY((p0 == p0_almost));
+        QVERIFY(p0 < p42);
+        QVERIFY(!(p42 < p0));
+        QVERIFY(!(p0 < p0));
     }
 };
 
-QTEST_MAIN(TestQString)
+QTEST_MAIN(core_logic_tests)
 
 #include "core_logic_tests.moc"
